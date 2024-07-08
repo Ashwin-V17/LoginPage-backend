f7f252da3e1aeb2e140c5c13909694514640ca93
Ashwin-V17 <ashwinv0017@gmail.com>
2024-07-08 10:12:42 +0530

    Update server.js to use environment variables

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(
-  "mongodb+srv://Test:<test123>@cluster1.htqinka.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
-);
-//guys, if mongo throws error, use mongoDb compass
+const mongoURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoURI, {})
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
 const UserSchema = new mongoose.Schema({
   username: String,
   password: String,
