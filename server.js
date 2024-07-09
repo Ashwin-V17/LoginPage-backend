209d4ebd6061208a0274fe7cc9e7930dea9c0cd4
Ashwin-V17 <ashwinv0017@gmail.com>
2024-07-09 15:43:02 +0530

    Update CORS configuration

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 const corsOptions = {
   origin: [
     "http://localhost:3000",
-    "https://login-page-frontend-4uh3gjy64-ashwin-vs-projects.vercel.app",
+    "https://login-page-frontend-m5zlycspq-ashwin-vs-projects.vercel.app",
   ],
   methods: "GET,POST",
   allowedHeaders: ["Content-Type", "Authorization"],
